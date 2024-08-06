909ee6037ff50b7ab2e8b607e9f94cca4da68fff
ganesh <ganeshsattineti123@gmaail.com>
2024-08-06 15:30:58 +0530

    add:object functions

---
+++ javascript/objectFunctions.js
@@ -0,0 +1,12 @@
+//object function:
+
+const person={
+    name:"ganesh",
+    address:{
+        city:"uppuluru"
+    },
+    fn:function(){
+        console.log("this is my object function");
+    }
+};
+person.fn();
