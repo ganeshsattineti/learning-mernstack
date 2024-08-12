19aa5665071168df352388bfd0fc426c53ea828e
ganesh <ganeshsattineti123@gmaail.com>
2024-08-12 23:01:15 +0530

    add: arthmetic operators

---
+++ javascript/arthmeticop.js
@@ -0,0 +1,57 @@
+//using + operator 
+var a=10;
+var b=20;
+var add =a+b;
+console.log(add);
+
+//using - opearator
+var x=20;
+var y=10;
+var sub =x-y;
+console.log(sub);
+
+//using * operator
+ var m1=10;
+ var m2=20;
+ var mul=m1*m2;
+ console.log(mul);
+
+ //using / opearator
+var d1=20;
+var d2=5;
+var div =d1/d2;
+console.log(div);
+
+//using % operator
+var md1=30;
+var md2=5;
+var mod=md1%md2;
+console.log(mod);
+
+//usimg ** square operator
+var c=10;
+var d=2;
+var result =c**d;
+console.log(result);
+
+//using ++ increment operator
+//post increment
+var i=10;
+i++
+console.log(i);
+
+//pre increment
+var j=20;
+++j;
+console.log(j);
+
+//using -- decrement operator
+//post decrement 
+var d=10;
+d--;
+console.log(d);
+
+//pre decrement
+var pd=10;
+--pd;
+console.log(pd);
