e9e391a2aee499f1a90fd560d1381d6a871bd91d
ganesh <ganeshsattineti123@gmaail.com>
2024-08-14 22:53:08 +0530

    add:assignment operator

---
+++ javascript/assignmentOp.js
@@ -0,0 +1,33 @@
+//assignment operator
+//('=')
+let x=10;
+
+//('+=')
+let a=10;
+a+=10;
+console.log(a);
+
+//('-=')
+let y=10;
+y-=5;
+console.log(y);
+
+//('*=')
+let m=10;
+m*=6;
+console.log(m);
+
+//('/=')
+let z=10;
+z/=2;
+console.log(z);
+
+//('%=')
+let b=20;
+b%=5;
+console.log(b);
+
+//('**=')
+let c=10;
+c**=2;
+console.log(c);
